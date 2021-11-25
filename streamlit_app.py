61c703eab5149171b2d27c12cadaac83af72b136
julian <julian.harley@gmail.com>
2021-11-25 14:41:08 -0400

    add simple streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,46 @@
-print('hello world')
+import streamlit as st
+import pandas as pd
+
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+})
+
+st.header("Awesome Data viz")
+with st.container():
+    with st.form("my_form"):
+        col1, col2 = st.columns(2)
+        st.header("Select resource")
+        with col1:
+            option = st.selectbox(
+                'Campaign Name',
+                df['first column'])
+            option = st.selectbox(
+                'Container',
+                df['first column'])
+            option = st.selectbox(
+                'Asset',
+                df['first column'])
+        with col2:
+            option = st.selectbox(
+                'Ad. BU',
+                df['first column'])
+            option = st.selectbox(
+                'Market',
+                df['first column'])
+            option = st.selectbox(
+                'Match Rate',
+                df['first column'])
+        submitted = st.form_submit_button("Submit")
+        if submitted:
+            st.write("option", option)
+
+            with st.container():
+                col1, col2 = st.columns(2)
+                with col1:
+                    st.header("DAB Sources")
+                    st.image("https://static.streamlit.io/examples/cat.jpg")
+
+                with col2:
+                    st.header("OMG Adapted Results")
+                    st.image("https://static.streamlit.io/examples/dog.jpg")
